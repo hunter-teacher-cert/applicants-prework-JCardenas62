4c3f7f7dd5fd9ab555712a17feb7c797fc9e6437
Jovani Cardenas <jac897@gmail.com>
2021-06-25 23:43:15 -0400

    added double to percentage,x, and midsec values

--- pre02/Time.java
+++ pre02/Time.java
@@ -3,9 +3,9 @@ public class Time {
     int hour = 21;
     int minute = 13;
     int second = 35;
-    int midsec = 24 *60 * 60;
-    int x = (hour * 60 * 60) + (minute * 60) + second;
-    double percentage= 76415.0 / 86400.0;
+    double midsec = 24 *60 * 60;
+    double x = (hour * 60 * 60) + (minute * 60) + second;
+    double percentage;
     int hourf = 22;
     int minf = 07;
     int secf = 25;
@@ -15,6 +15,7 @@ public class Time {
     System.out.print("The number of seconds remaining in the day is: ");
     System.out.println((midsec) - ((hour * 60 * 60) + (minute * 60) + second));
     System.out.print("The perecentage of day that has passed is: ");
+    percentage = x / midsec;
     System.out.printf("%.2f", (percentage) * 100);
     System.out.println("%");
     System.out.print("The number of seconds elapsed since the beginning of this exercise: ");
