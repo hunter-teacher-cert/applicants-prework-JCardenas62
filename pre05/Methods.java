1a840d65fc9fa202f6860bbcb7ae5b551301ac3b
Jovani Cardenas <jac897@gmail.com>
2021-06-27 14:30:32 -0400

    completed exercise 8

--- pre05/Methods.java
+++ pre05/Methods.java
@@ -13,6 +13,7 @@ public class Methods{
   public static void main(String[] args){
     System.out.println(isDivisible(4, 2));
     System.out.println(isTriangle(1,3,4));
+    System.out.println(ack(3,3));
 
   }
 //Exercise 3: methods isTriangle
@@ -23,6 +24,17 @@ public static boolean isTriangle (int a, int b, int c){
           return true;
       }
   }
-//Exercise 8: methods ack m=0 n=0 and m>0 n>0
+//Exercise 8: Ackermann
+  public static int ack (int m, int n) {
+    if (m==0) {
+      return n+1;
+    } else if (m>0 && n==0){
+        return (ack (m-1, 1));
+    } else if (m > 0 && n > 0) {
+        return (ack (m - 1, ack (m, n - 1)));
+    } else {
+      return (-2);
+    }
+  }
 
 }
